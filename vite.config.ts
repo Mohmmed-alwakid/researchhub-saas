a94666f713cecb8722e13f8b4987bc8de7e88496
Mohmmed-alwakid <mza0137@tigermail.auburn.edu>
2025-05-30 19:41:22 +0300

    Fix: Vite config - add explicit root path to resolve build entry module issue

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: '.',
   mode: 'production',
   define: {
     'process.env.NODE_ENV': JSON.stringify('production')
